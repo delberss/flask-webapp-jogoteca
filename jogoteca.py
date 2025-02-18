b2792bc6f96ee59a4ad030cd9dcfce0c0749772b
delberss <delberss@hotmail.com>
2025-02-18 17:39:29 -0300

    Deixando o código html dinâmico

--- jogoteca.py
+++ jogoteca.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__) #__name__ referencia a propria pagina
 
+
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    lista_jogos=['Batman', 'Fifa']
+    return render_template('lista.html', titulo='Jogos', lista_jogos=lista_jogos)
 
-app.run()
+app.run(debug=True) #debug=True para toda vez que tiver mudança no código, Flask atualiza

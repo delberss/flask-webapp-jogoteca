a2b26d8be7fff7ff86e079328275bfb9bebdcf5d
delberss <delberss@hotmail.com>
2025-02-18 17:57:08 -0300

    Utilizando classe para criar novos Jogos e novas colunas

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from Jogo import Jogo
 
 app = Flask(__name__) #__name__ referencia a propria pagina
 
 
 @app.route('/inicio')
 def ola():
-    lista_jogos=['Batman', 'Fifa']
+    jogo1 = Jogo('Fifa', 'Esporte', 'PS5/XBOX')
+    jogo2 = Jogo('Call of Duty', 'FPS ', 'PC')
+    jogo3 = Jogo('Counter-Strike', 'FPS ', 'PC')
+
+    lista_jogos = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', lista_jogos=lista_jogos)
 
 app.run(debug=True) #debug=True para toda vez que tiver mudança no código, Flask atualiza

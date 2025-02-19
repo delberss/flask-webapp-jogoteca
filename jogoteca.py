74a637088cdaa5f358db9243a802a48a85b6166a
delberss <delberss@hotmail.com>
2025-02-19 15:27:23 -0300

    Criação de rotas de login e logout

--- jogoteca.py
+++ jogoteca.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from Jogo import Jogo
-
-app = Flask(__name__) #__name__ referencia a propria pagina
 
 jogo1 = Jogo('Fifa', 'Esporte', 'PS5/XBOX')
 jogo2 = Jogo('Call of Duty', 'FPS ', 'PC')
 jogo3 = Jogo('Counter-Strike', 'FPS ', 'PC')
 
 lista_jogos = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__) #__name__ referencia a propria pagina
+app.secret_key = 'dssoares'
 
 @app.route('/')
 def index():
@@ -25,6 +26,29 @@ def criar():
     jogo = Jogo(nome,categoria,console)
     lista_jogos.append(jogo)
     return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    if 'admin' == senha:
+        session['usuario_logado'] = usuario
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado.')
+        return redirect('/login')
 
 
 app.run(debug=True) #debug=True para toda vez que tiver mudança no código, Flask atualiza

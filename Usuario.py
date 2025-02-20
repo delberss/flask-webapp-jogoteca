b3a39721bbe3b1ced7c18d58dd0ab1b38f31d405
delberss <delberss@hotmail.com>
2025-02-20 20:55:20 -0300

    Autenticação de usuário - Possibilita cadastrar novos jogos

---
+++ Usuario.py
@@ -0,0 +1,6 @@
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+    
--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 from Jogo import Jogo
+from Usuario import Usuario
 
 jogo1 = Jogo('Fifa', 'Esporte', 'PS5/XBOX')
 jogo2 = Jogo('Call of Duty', 'FPS ', 'PC')
 jogo3 = Jogo('Counter-Strike', 'FPS ', 'PC')
 
 lista_jogos = [jogo1, jogo2, jogo3]
+
+usuario1 = Usuario('Delber', 'delberss', '123')
+usuario2 = Usuario('Pantera', 'panterass', '123')
+usuario3 = Usuario('Messi', 'leomessi', 'barça')
+
+usuarios = {
+    usuario1.nickname: usuario1,
+    usuario2.nickname: usuario2,
+    usuario3.nickname: usuario3,
+}
 
 app = Flask(__name__) #__name__ referencia a propria pagina
 app.secret_key = 'dssoares'
@@ -45,20 +56,24 @@ def logout():
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    usuario = request.form['usuario']
-    senha = request.form['senha']
+    usuario_form = request.form['usuario']
+    senha_form = request.form['senha']
     proxima_pagina = request.form['proxima']
 
     if not proxima_pagina or proxima_pagina == "None":
         proxima_pagina = url_for('index')
 
-    if senha == 'admin':
-        session['usuario_logado'] = usuario
-        flash(session['usuario_logado'] + ' logado com sucesso!')
-        return redirect(proxima_pagina)
+    if usuario_form in usuarios:
+        usuario = usuarios[usuario_form]
+        if senha_form == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname.capitalize() + ' logado com sucesso!')
+            return redirect(proxima_pagina)
+        else:
+            flash('Usuário não logado. Senha incorreta')
+            return redirect(url_for('login'))
     else:
-        flash('Usuário não logado.')
+        flash('Usuário não cadastrado.')
         return redirect(url_for('login'))
-
 
 app.run(debug=True) #debug=True para toda vez que tiver mudança no código, Flask atualiza
